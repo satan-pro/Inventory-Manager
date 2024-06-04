ac06ae7244678154bd56487105bf18caa4e24646
Sayantan <121564810+satan-pro@users.noreply.github.com>
2024-06-04 22:44:36 +0530

    Update cloudinary.js

--- server/utils/cloudinary.js
+++ server/utils/cloudinary.js
@@ -2,9 +2,9 @@ const cloudinary = require('cloudinary').v2;
 require('dotenv').config();
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+    cloud_name: "dxdn0myge",
+    api_key: "913636372551487",
+    api_secret: "uL2yrV4_mItdW_sY-71gYc0_skg"
 });
 
 module.exports = cloudinary;
